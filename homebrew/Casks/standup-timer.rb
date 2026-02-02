650de04375abfe29f29f194e0ad7152cb4ea9d09
Anshu Prateek <github.com@anshuprateek.com>
2026-02-02 15:45:43 +0530

    fix: correct Homebrew formula URL and SHA256 for release assets

--- homebrew/Casks/standup-timer.rb
+++ homebrew/Casks/standup-timer.rb
@@ -1,8 +1,8 @@
 cask "standup-timer" do
   version "2.0.0"
-  sha256 "bf811bec081ff08fc70a758d36da7dfb4766000cd162ee93df574a7987f0c022"
+  sha256 "ef11d19705b448344a5bced45ae9d2e5b78556022ae6c4f7548a951a1b4b62b6"
 
-  url "https://github.com/anshprat/standup-timer/releases/download/v#{version}/Standup%20Timer-#{version}-arm64-mac.zip"
+  url "https://github.com/anshprat/standup-timer/releases/download/v#{version}/Standup.Timer-#{version}-arm64-mac.zip"
   name "Standup Timer"
   desc "Standup meeting timer overlay for team meetings"
   homepage "https://github.com/anshprat/standup-timer"
